5e5f2441cc69d6505cb82adeea647368d31e9af3
Benjamin Kim <benjaminsangminkim@gmail.com>
2021-05-05 11:48:51 -0400

    First commit

---
+++ BenfordsLaw.java
@@ -0,0 +1,15 @@
+/*
+ * Date: May 5, 2021
+ * Name: David Han, Benjamin Kim
+ * Teacher: Mr. Ho
+ * Description: BenfordsLaw Assignment
+ */
+ 
+class BenfordsLaw {
+    public static void main(String[] args) {
+
+    }
+    public static void readFile(String fileName) {
+        
+    }
+}
