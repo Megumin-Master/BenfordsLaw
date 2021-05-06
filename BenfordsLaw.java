b64a9c23a734a6fe32678c0edb7218dfa46b2ee3
Benjamin Kim <benjaminsangminkim@gmail.com>
2021-05-06 11:50:31 -0400

    Big W

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -9,51 +9,53 @@ import java.io.*;
 
 class BenfordsLaw {
     public static void main(String[] args) {
-        firstNum("sales.csv");
-    }
-    public static int dataNum(String fileName) {
+        String fileName = "sales.csv";
         File file = new File(fileName);
+        firstNum(file);
+    }
+    public static int dataNum(File file) {
         String line = "";
         int lineCount = 0;
-
+       
         try {
             Scanner scan = new Scanner(file);
             while(scan.hasNextLine()) {
                 line = scan.nextLine();
-                if (Character.isDigit(line.charAt(5))) {
+                if (Character.isDigit(line.charAt(4))) {
                 //System.out.println(line);
                 lineCount++;
                 }
             }
-            scan.close();
             return lineCount;
-            //System.out.println(lineCount);
         } catch (FileNotFoundException e) {
             System.out.println("An error has occured");
         }
         return 0;
+        //System.out.println(lineCount);
     }
     
-    public static int[] firstNum(String fileName) {
-        File file = new File(fileName);
-        int lineCount = dataNum(fileName);
+    public static int[] firstNum(File file) {
+        int lineCount = dataNum(file);
         int[] firstNum = new int[lineCount];
         String line = "";
         char first = ' ';
     
         try {
             Scanner scan = new Scanner(file);
-            for(int i = 0; i <= lineCount; i++) {
-                line = scan.nextLine();
-                if (Character.isDigit(line.charAt(5)) == false) {
+            for(int i = 0; i < lineCount; i++) {
+                if (scan.hasNextLine()) {
                     line = scan.nextLine();
                 }
-                first = line.charAt(5);
+                if (Character.isDigit(line.charAt(4)) == false) {
+                    if (scan.hasNextLine()) {
+                        line = scan.nextLine();
+                    }
+                }
+                first = line.charAt(4);
                 int num = Character.getNumericValue(first);
                 firstNum[i] = num;
-                //System.out.println(firstNum[i]);
+                System.out.println(firstNum[i]);
             }
-            scan.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error has occured");
         }
