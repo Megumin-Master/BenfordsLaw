47b61f5fc167c9b0218b9b899a5dd9b46928a04c
Benjamin Kim <benjaminsangminkim@gmail.com>
2021-05-05 23:02:57 -0400

    New method

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -9,9 +9,9 @@ import java.io.*;
 
 class BenfordsLaw {
     public static void main(String[] args) {
-
+        firstNum("sales.csv");
     }
-    public static void readFile(String fileName) {
+    public static int readFile(String fileName) {
         File file = new File(fileName);
         String line = "";
         int lineCount = 0;
@@ -20,8 +20,32 @@ class BenfordsLaw {
             Scanner scan = new Scanner(file);
             while(scan.hasNextLine()) {
                 line = scan.nextLine();
-                System.out.println(line);
+                //System.out.println(line);
                 lineCount++;
+            }
+            scan.close();
+            return lineCount;
+            //System.out.println(lineCount);
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occured");
+        }
+        return 0;
+    }
+    
+    public static void firstNum(String fileName) {
+        File file = new File(fileName);
+        int lineCount = readFile(fileName);
+        int[] firstNum = new int[lineCount];
+        String line = "";
+    
+        try {
+            Scanner scan = new Scanner(file);
+            for(int i = 0; i < lineCount; i++) {
+                line = scan.nextLine();
+                char first = line.charAt(5);
+                int num = Character.getNumericValue(first);
+                firstNum[i] = num;
+                System.out.println(firstNum[i]);
             }
             scan.close();
         } catch (FileNotFoundException e) {
