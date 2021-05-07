cb8f012071b659a5c5400ed3a16ce8d0f0c7bde9
David <david4han@gmail.com>
2021-05-07 18:32:22 -0400

    Basic Layout of methods

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -11,4 +11,12 @@ class BenfordsLaw {
     public static void main(String[] args) {
 
     }
+
+    public static int[] sortArray(){
+
+    }
+
+    public static float percentage(){
+        
+    }
 }
